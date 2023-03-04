ef49cc9b99e5e2a6e71c82e213d8759aca8ec634
Artjoms Tatagaškins <125375444+ArtjomsTatagaskins@users.noreply.github.com>
2023-03-04 08:09:49 +0000

    committed2

--- tree_height.py
+++ tree_height.py
@@ -6,24 +6,27 @@ import numpy
 
 
 def compute_height(n, parents):
-    heights = [0] * n
-    max_height = 0
-    queue = []
+    tree = [[]for i in range (n)]
+    root = -1
     for i in range(n):
         parent = parents[i]
         if parent == -1:
-            heights[i] = 1
-            queue.append(i)
+            root = i
+        else:
+            tree[parent].append(i)
+
+    height = 0
+    queue = [root]
+
     while queue:
-        nod = queue.pop(0)
-        height = heights[nod]
-        for i in range(n):
-            if parents[i] == nod:
-                heights[i] = height + 1
-                queue.append(i)
-                if height + 1 > max_height:
-                     max_height = heights[i]
-    return max_height
+        size = len(queue)
+        for i in range(size):
+            node = queue.pop(0)
+            for child in tree[node]:
+                queue.append(child)
+        height += 1    
+
+    return height
 
 
 def main():
@@ -35,6 +38,4 @@ def main():
 
 if __name__ == '__main__': 
     sys.setrecursionlimit(10**7)  
-    threading.stack_size(2**27)  
-    threading.Thread(target=main).start()
-# print(numpy.array([1,2,3]))
+    main()
